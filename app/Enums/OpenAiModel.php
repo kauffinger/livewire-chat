c3b994d4218c5201931f25348c31443192da46f0
Konstantin Auffinger <62616071+kauffinger@users.noreply.github.com>
2025-06-10 22:47:45 +0200

    chore: add o3-mini

--- app/Enums/OpenAiModel.php
+++ app/Enums/OpenAiModel.php
@@ -8,11 +8,9 @@ enum OpenAiModel: string
     case Gpt4 = 'gpt-4';
     case Gpt4oMini = 'gpt-4o-mini';
     case Gpt4o = 'gpt-4o';
-
     case o3 = 'o3-2025-04-16';
-
+    case o3Mini = 'o3-mini-2025-01-31';
     case o4Mini = 'o4-mini-2025-04-16';
-
     case Gpt41nano = 'gpt-4.1-nano-2025-04-14';
 
     /**
@@ -28,6 +26,7 @@ enum OpenAiModel: string
             self::Gpt4oMini->value => 'gpt-4o-mini',
             self::Gpt4o->value => 'gpt-4o',
             self::o3->value => 'o3-2025-04-16',
+            self::o3Mini->value => 'o3-mini-2025-01-31',
             self::o4Mini->value => 'o4-mini-2025-04-16',
             self::Gpt41nano->value => 'gpt-4.1-nano-2025-04-14',
         ];
